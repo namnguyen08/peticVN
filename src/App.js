1716567bf5151069f5aa0abb35b45a256163f8b6
namnguyen08 <namnv8889@gmail.com>
2020-05-31 08:28:27 +0800

    update menu items to navigate with withRouter - history

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import HomePage from './pages/pages/homepage.component';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div> 
-      <HomePage/>
+    <div>
+      <Switch>
+        <Route path='/' component={HomePage} />
+      </Switch>
     </div>
   );
 }
